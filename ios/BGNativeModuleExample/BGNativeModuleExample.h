06e6cc3a5bedfda89ae5c88f499dbf4aa0e1561d
kevinxuelei <kevinxuelei@163.com>
2021-08-22 08:07:53 +0800

    add project

---
+++ ios/BGNativeModuleExample/BGNativeModuleExample.h
@@ -0,0 +1,12 @@
+//
+//  BGNativeModuleExample.h
+//  BGNativeModuleExample
+//
+//  Created by dongxuelei on 2021/8/21.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+@interface BGNativeModuleExample : NSObject<RCTBridgeModule>
+
+@end
